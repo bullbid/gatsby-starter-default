844c8f282d2d209086d2ed0cf28bb9583ab3159e
Gastón Irazuzta <gastonirazuzta@gmail.com>
2022-05-27 14:58:02 -0300

    Home con fondo, logo, slogan y cartel informativo de "sitio en construcción"

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+//import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -24,29 +24,31 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+        <>
+            {/*<Header siteTitle={data.site.siteMetadata?.title || `Title`} />*/}
+            <div style = {
+                    {
+                        margin: `0`,
+                        maxWidth: `var(--size-content)`,
+                        padding: `var(--size-gutter)`,
+                    }
+            }>
+                <main>{children}</main>
+                {/*
+                <footer style={
+                    {
+                        marginTop: `var(--space-5)`,
+                        fontSize: `var(--font-sm)`,
+                    }
+                }>
+                    © {new Date().getFullYear()} &middot; Built with
+                    {` `}
+                    <a href="https://www.gatsbyjs.com">Gatsby</a>
+                </footer>
+                */}
+            </div>
+        </>
+    )
 }
 
 Layout.propTypes = {
